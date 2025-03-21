7f578d09e55d55309531d8aea340f0a5292b43e7
Jayesh <jayeshthakur47@gmail.com>
2025-03-21 18:50:39 +0900

    updated linting

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,21 +1,21 @@
 module.exports = {
   root: true,
-  extends: "airbnb-base",
+  extends: 'airbnb-base',
   env: {
     browser: true,
   },
-  parser: "@babel/eslint-parser",
+  parser: '@babel/eslint-parser',
   parserOptions: {
     allowImportExportEverywhere: true,
-    sourceType: "module",
+    sourceType: 'module',
     requireConfigFile: false,
   },
   rules: {
-    "import/extensions": ["error", { js: "always" }], // require js file extensions in imports
-    "linebreak-style": [
-      "error",
-      process.platform === "win32" ? "windows" : "unix",
-    ], //allow windows if platform is windows when developing locally and unix for unix machines and GH actions
-    "no-param-reassign": [2, { props: false }], // allow modifying properties of param
+    'import/extensions': ['error', { js: 'always' }], // require js file extensions in imports
+    'linebreak-style': [
+      'error',
+      process.platform === 'win32' ? 'windows' : 'unix',
+    ], // allow windows if platform is windows when developing locally and unix for  GH actions
+    'no-param-reassign': [2, { props: false }], // allow modifying properties of param
   },
 };
