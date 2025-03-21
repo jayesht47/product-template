36cad25758daf0c9756a3b6845dd04ab2b5d1de7
Jayesh <jayeshthakur47@gmail.com>
2025-03-21 18:45:36 +0900

    made linebreak style dynamic based on platform

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,18 +1,21 @@
 module.exports = {
   root: true,
-  extends: 'airbnb-base',
+  extends: "airbnb-base",
   env: {
     browser: true,
   },
-  parser: '@babel/eslint-parser',
+  parser: "@babel/eslint-parser",
   parserOptions: {
     allowImportExportEverywhere: true,
-    sourceType: 'module',
+    sourceType: "module",
     requireConfigFile: false,
   },
   rules: {
-    'import/extensions': ['error', { js: 'always' }], // require js file extensions in imports
-    'linebreak-style': ['error', 'unix','windows'], //allow windows and unix linebreaks
-    'no-param-reassign': [2, { props: false }], // allow modifying properties of param
+    "import/extensions": ["error", { js: "always" }], // require js file extensions in imports
+    "linebreak-style": [
+      "error",
+      process.platform === "win32" ? "windows" : "unix",
+    ], //allow windows if platform is windows when developing locally and unix for unix machines and GH actions
+    "no-param-reassign": [2, { props: false }], // allow modifying properties of param
   },
 };
